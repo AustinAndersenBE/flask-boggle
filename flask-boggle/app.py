91b7e9100409fb696fd75bc349fafb92141ac16d
Austin <senhua@DESKTOP-5CE9SR5>
2023-10-06 18:17:42 -0700

    added docstrings

--- flask-boggle/app.py
+++ flask-boggle/app.py
@@ -9,6 +9,11 @@ boggle_game_instance = Boggle() # create instance of Boggle
 
 @app.route('/')
 def display_boggle_board():
+    """Display Boggle board and store it in session, and renders it.
+
+    Returns:
+        str: HTML content of the Boggle board
+    """
     generated_board = boggle_game_instance.make_board()
     session['current_boggle_board'] = generated_board
     session.setdefault('highest_score', 0)
@@ -17,6 +22,12 @@ def display_boggle_board():
 
 @app.route('/submit_user_guess', methods=['POST'])
 def submit_user_guess():
+    """Handles submission of user's word guess.
+    Validates if submitted word is on board and words.txt.
+
+    Returns:
+        JSON: a dictionary with key 'result' and value of result of validation
+    """
     user_guess = request.json['user_guess'] #get value associated with key 'user_guess' from JSON data
     current_board = session['current_boggle_board']
     
@@ -25,6 +36,13 @@ def submit_user_guess():
 
 @app.route('/submit_final_score', methods=['POST'])
 def submit_final_score():
+    """Handle the submission of the final score when the game ends.
+    
+    Updates session data for the highest score and times played.
+    
+    Returns:
+        JSON: a dictionary with keys 'highest_score' and 'times_played' and their values.
+    """
     final_score = request.json['final_score']
     session['times_played'] += 1
 
